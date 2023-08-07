802a1c3d4de33424103434bef0ccd740ea187119
Shaylan Govender <keyanoshay@gmail.com>
2023-08-07 12:39:30 -0700

    part1

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/bin/bash
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
+
